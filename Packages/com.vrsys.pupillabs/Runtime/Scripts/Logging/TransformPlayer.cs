401f6bd4c30563200649766da745de2760641869
Gareth Rendle <gary.444@hotmail.co.uk>
2026-08-11 21:55:05 +0200

    Update TransformPlayer.cs

--- Packages/com.vrsys.pupillabs/Runtime/Scripts/Logging/TransformPlayer.cs
+++ Packages/com.vrsys.pupillabs/Runtime/Scripts/Logging/TransformPlayer.cs
@@ -38,6 +38,8 @@ namespace VRSYS.PupilLabs
         public bool IsLoaded { get; private set; }
         public bool IsPlaying { get; private set; }
 
+        public double PlaybackOrigin => playbackOrigin;
+
         private void Start()
         {
             if (playOnStart)
